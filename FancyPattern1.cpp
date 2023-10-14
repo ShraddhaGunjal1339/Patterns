518fd68a75413e2df5d4b4b0967e24dd8c5ca3e3
Shraddha <shraddhagunjal1037@gmail.com>
2023-10-14 09:39:29 +0530

    Pattern1

---
+++ FancyPattern1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+/*
+
+Give number 
+5
+
+********1********
+*******2*2*******
+******3*3*3******
+*****4*4*4*4*****
+****5*5*5*5*5****
+
+
+*/
+    int n; 
+    cout << "Give number "<< endl;
+    cin >> n ;
+
+    for (int i = 0 ; i < n ; i++){
+        int start_num_index = 8-i;
+        int num =i+1;
+        int count_num = num ;
+        for (int j = 0 ; j < 17 ; j++){
+            if(j == start_num_index && count_num > 0){
+                cout <<  num;
+                start_num_index += 2;
+                count_num --;
+            }
+            else{
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
