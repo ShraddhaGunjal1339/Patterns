b7d0d5728a39e382005d7412147ca4fbc713e35b
Shraddha <shraddhagunjal1037@gmail.com>
2023-10-21 09:33:15 +0530

    Assert

--- FancyPattern1.cpp
+++ FancyPattern1.cpp
@@ -20,10 +20,27 @@ Give number
     int n; 
     cout << "Give number "<< endl;
     cin >> n ;
-    if(n > 9 ){
-        cout << "Plese put value less than 9. " << endl;
-        return 0;
-    }
+    // if(n > 9 ){
+    //     cout << "Plese put value less than 9. " << endl;
+    //     return 0;
+    // }
+
+
+    assert(n<=9);
+
+
+    /*
+    
+        Assertions are statements used to test assumptions made by programmers. For example, we may use assertion to check if the pointer returned by malloc() is NULL or not. 
+    Following is the syntax for assertion.
+    
+
+    void assert( int expression ); 
+    If the expression evaluates to 0 (false), then the expression, sourcecode filename, and line number are sent to the standard error, and then abort() function is called. 
+    For example, consider the following program. 
+    
+    */
+
     for (int i = 0 ; i < n ; i++){
         int start_num_index = 8-i;
         int num =i+1;
